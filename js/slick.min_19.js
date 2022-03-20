0d8c2ffb23b1a34040de253dab0d338003efe0d4
Muhammad Muslih W <muslihwaskito@gmail.com>
2022-03-20 21:00:38 +0700

    initial commit

---
+++ js/slick.min_19.js
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 | DOGEPUNK</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../images/favicon_203.png" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link href="../css/css.css" rel="stylesheet">
+    <link href="../css/css_1.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="../css/font-awesome.min_203.css">
+    <!-- Bootstrap-->
+    <link rel="stylesheet" href="../css/bootstrap.min_203.css">
+    <!-- Style -->
+    <link rel="stylesheet" href="../css/style_203.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" href="../css/responsive_203.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="../css/animate.min_203.css">
+    <!-- Light Box -->
+    <link href="../css/lightbox_203.css" rel="stylesheet">
+    <!-- Video js -->
+    <link href="../css/video-js_203.css" rel="stylesheet">
+    <!-- Datapicker -->
+    <link href="../css/daterangepicker.min_203.css" rel="stylesheet">
+    <!-- Sliders -->
+    <link rel="stylesheet" type="text/css" href="../css/slick_203.css">
+    <link rel="stylesheet" type="text/css" href="../css/slick-theme_203.css">
+</head>
+
+<body>
+    <!-- PRELOADER START -->
+    <div class="loader-wrapper">
+        <div class="cssload-loader">
+            <div class="cssload-inner cssload-one"></div>
+            <div class="cssload-inner cssload-two"></div>
+            <div class="cssload-inner cssload-three"></div>
+        </div>
+    </div>
+    <!-- PRELOADER END -->
+    <!-- HEADER START -->
+    <div class="header-line-wrapper">
+        <header class="header-wrapper fixed-top plr100">
+            <div class="table height-100p">
+                <div class="table-row">
+                    <div class="table-cell valign-middle text-left">
+                        <a href="index.html" class="logo">
+                            <img src="../images/logo_203.png" alt class="img-responsive inline-block">
+                        </a>
+                    </div>
+                    <div class="table-cell valign-top text-center vm-sm">
+                        <div class="main-menu">
+                            <span class="toggle_menu">
+                                <span></span>
+                            </span>
+                            <ul class="menu clearfix">
+                                <li class="inline-block active">
+                                    <a href="index.html">
+                                        Home
+                                    </a>
+                                </li>
+    
+                                <li class="inline-block menu-item-has-children">
+                                    <a href="index.html">
+                                        WHAT IS
+                                    </a>
+    
+                                </li>
+                                <li class="inline-block menu-item-has-children">
+                                    <a href="index.html">
+                                        tokenomics
+                                    </a>
+    
+                                </li><li class="inline-block menu-item-has-children">
+                                    <a href="index.html">
+                                        MOONMAP
+                                    </a>
+                                </li>
+                                <li class="inline-block menu-item-has-children">
+                                    <a>
+                                        FEATURE
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="/nftgallery.html">NFT</a>
+                                        </li>
+                                        <li>
+                                            <a href>Mint NFT</a>
+                                        </li>
+                                        <li>
+                                            <a href="/swap.html">Staking</a>
+                                        </li>
+                                        <li>
+                                            <a href="#game">Game</a>
+                                        </li>
+                                    </ul>
+                                </li>
+    
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="table-cell valign-top text-right">
+                        <div class="right-bl">
+                            <div class="search-wrapper inline-block valign-middle">
+                                <i class="fa fa-search color-white fsize-28" aria-hidden="true"></i>
+                                <form class="search-content" action="#">
+                                    <input type="text" placeholder="Search Keyword...">
+                                    <button class="color-white">
+                                        <i class="fa fa-search" aria-hidden="true"></i>
+                                    </button>
+                                </form>
+                            </div>
+                            <a href="/swap.html" class="btn header-btn ml25 color-white hidden-sm hidden-xs">
+                                dogepunk swap
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+    <!-- HEADER END -->
+    <!-- SECTION START -->
+    <section class="table page-404">
+        <div class="table-row">
+            <div class="table-cell valign-middle text-center">
+                <div>
+                    <img src="../images/404-img_202.png" alt>
+                </div>
+                <div class="title-error mt30">
+                    SORRY, PAGE NOT FOUND!
+                </div>
+            </div>
+        </div>
+        <div class="footer-line table-row">
+            <div class="footer-404 table-cell valign-middle">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        DOGEPUNK © Copyright 2022 
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right">
+                            All Rights Reserved
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- SECTION END -->    
+    <!-- Scripts -->
+    <script src="jquery-3.3.1.min_203.js"></script>
+    <script src="bootstrap.min_203.js"></script>
+    <script src="slick.min_203.js"></script>
+    <script src="flexmenu.min_203.js"></script>
+    <script src="jquery.matchHeight_203.js"></script>
+    <script src="muuri_203.js"></script>
+    <script src="jquery.vide.min_203.js"></script>
+    <script src="jquery.paroller.min_203.js"></script>
+    <script src="video_203.js"></script>
+    <script src="jquery.inview.min_203.js"></script>
+    <script src="progressbar.min_203.js"></script>
+    <script src="lightbox_203.js"></script>
+    <script src="moment.min_203.js"></script>
+    <script src="jquery.daterangepicker.min_203.js"></script>
+    <script src="wow.min_203.js"></script>
+    <script src="script_203.js"></script>
+</body>
+
+</html>
